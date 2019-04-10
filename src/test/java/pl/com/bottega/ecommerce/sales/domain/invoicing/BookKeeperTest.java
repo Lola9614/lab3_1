b974ef07432faa36a2fdea2c62ee1b3da34a2e35
Karolina Płóciennik <209434@edu.p.lodz.pl>
2019-04-10 22:41:26 +0200

    isolation test return one inovice if one is requested

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -0,0 +1,40 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Matchers;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class BookKeeperTest {
+
+    @Test
+    public void shouldReturnOneInvoiceIfOneIsRequested() {
+
+        BookKeeper bookKeeper = new BookKeeper(new InvoiceFactory());
+        InvoiceRequest invoiceRequest = new InvoiceRequest(null);
+
+        ProductData productData = Mockito.mock(ProductData.class);
+        Mockito.when(productData.getType()).thenReturn(ProductType.STANDARD);
+
+        invoiceRequest.add(new RequestItem(productData,1,new Money(1)));
+
+        TaxPolicy taxPolicy = mock(TaxPolicy.class);
+        when(taxPolicy.calculateTax(Matchers.any(ProductType.class),Matchers.any(Money.class)))
+                .thenReturn(new Tax(new Money(1),null));
+
+        Invoice responseInvoice = bookKeeper.issuance(invoiceRequest,taxPolicy);
+
+        assertThat(responseInvoice.getItems().size(),is(1));
+    }
+}
